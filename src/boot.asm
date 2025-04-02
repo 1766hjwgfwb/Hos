4494459fcf73ea33674fa5eeb2f14bf95de8e623
hjwgfwb <hjwgfwb@gmail.com>
2025-04-02 16:48:03 +0800

    内核加载器

--- src/boot.asm
+++ src/boot.asm
@@ -19,23 +19,26 @@ mov si, booting
 call print
 
 
-xchg bx, bx ;　断点
+
+
+; jmp error test
+
 
 mov edi, 0x1000 ; 内存地址
-mov ecx, 0 ; 起始扇区
-mov bl, 1 ; 扇区数量
+mov ecx, 2 ; 起始扇区
+mov bl, 4 ; 扇区数量
 
 call read_disk ; 读取硬盘
 
 xchg bx, bx ;　断点
 
-mov edi, 0x1000 ; 内存地址
-mov ecx, 2; 起始扇区
-mov bl, 1 ; 扇区数量
 
-call write_disk ; 写入硬盘
+; 比较地址魔数
+cmp word [0x1000], 0xaa55
+jnz error ; 地址魔数不匹配
 
-xchg bx, bx ;　断点
+
+jmp 0:0x1002 ; 跳转到内核入口
 
 
 
@@ -114,75 +117,14 @@ read_disk:
         ret
 
 
-write_disk:
 
-    ; 设置读写扇区的数量
-    mov dx, 0x1f2;
-    mov al, bl
-    out dx, al
+error:
+    mov si, .msg
+    call print
+    hlt ; 死机
+    jmp $
+    .msg db "Booting error!", 10, 13, 0; \n\r
 
-    inc dx ; 0x1f3
-    mov al, cl ; 起始扇区的前8位
-    out dx, al
-
-    inc dx ; 0x1f4
-    shr ecx, 8
-    mov al, cl ; 起始扇区的中8位
-    out dx, al
-
-    inc dx ; 0x1f5
-    shr ecx, 8
-    mov al, cl ; 起始扇区的高8位
-    out dx, al
-
-    inc dx ; 0x1f6
-    shr ecx, 8
-    and cl, 0b1111 ;将高四位置0
-
-    mov al, 0b1110_0000
-    or al, cl
-    out dx, al ; 主盘 - LBA模式
-
-    inc dx ; 0x1f7
-    mov al, 0x30 ; 写硬盘
-    out dx, al
-
-    xor ecx, ecx ; 清空ecx
-    mov cl, bl ; 扇区数量
-
-    .write:
-        push cx ; 保存 cx
-        call .writes ; 写入一个扇区
-        call .waits ; 等待数据准备完毕
-        pop cx ; 恢复 cx
-        loop .write
-
-    ret
-
-    .waits:
-        mov dx, 0x1f7 ; 选择主盘
-        .check:
-            in al, dx
-            jmp $+2; nop 跳转到下一行
-            jmp $+2; 延迟
-            jmp $+2
-            and al, 0b1000_1000
-            cmp al, 0b0000_1000
-            jnz .check ; 等待准备完毕
-        ret
-
-    .writes:
-        mov dx, 0x1f0 ; 选择主盘
-        mov cx, 256 ; 一个扇区 256字(512字节)
-        .writew:
-            mov ax, [edi] ; 读出内存
-            out dx, ax ; 写出硬盘
-            jmp $+2; nop 跳转到下一行
-            jmp $+2; 延迟
-            jmp $+2
-            add edi, 2 ; 指向下一个字
-            loop .writew
-        ret
 
 
 print:
